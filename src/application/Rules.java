229f39214d18da704b540d910bb89e9637013f6d
kundan_pawar <kundanp0001@gmail.com>
2023-10-12 18:54:20 +0530

    quiz app done

---
+++ src/application/Rules.java
@@ -0,0 +1,72 @@
+package application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Try to attempt all of the Question , there will be no reward or punishment for attempting." + "<br><br>" +
+                "2. There is No Negative Marking in the Quiz." + "<br><br>" +
+                "3. Questions are from Core Java only , do revise them if You want to." + "<br><br>" +
+                "4. There Exist one 50:50 Lifeline Available throughout the Quiz." + "<br><br>" +
+                "5. Try To attempt all Questions Fastly as there is only 15 sec. time for each Question." + "<br><br>" +
+                "6. Do Not refresh the Page,This will vanishes your all previous Progress." + "<br><br>" +
+                "7. You are allowed to skip the question ." + "<br><br>" +
+                "8. Have faith in Yourself , Good luck." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
